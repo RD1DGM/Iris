e50dd90e32325c8d4fe95a110fab901f2e821cfe
Rafael Mariscal <dominicmariscal78@gmail.com>
2020-02-02 22:00:34 -0500

    updates

--- iris-client/src/Components/Section_2.js
+++ iris-client/src/Components/Section_2.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from "react";
 
 function Landing2Text() {
   return (
     <>
-      <p className="p_4" style={{ fontSize: '2.5em', fontWeight: 'bold' }}>
-        Iris has a native token called OMEN.
+      <p
+        className="p_4"
+        style={{ fontSize: "calc(1em + 3vw)", fontWeight: "bold" }}
+      >
+        Iris has a native token called{" "}
+        <span style={{ color: "hsl(213, 70%, 40%)", fontWeight: 900 }}>
+          OMEN
+        </span>
+        .
       </p>
       <br />
-      <p className="p_5">
-        You can earn tokens by interacting with the DApp or earn token instantly by creating, sharing, and commenting to
-        posts.
+      <p
+        className="p_5"
+        style={{ lineHeight: 1.5, fontSize: " calc(1em + .35vw)" }}
+      >
+        You can earn tokens by interacting with the DApp or earn token instantly
+        by creating, sharing, and commenting to posts.
       </p>
     </>
   );
